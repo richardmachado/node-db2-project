973693bd86e23236c97379dce9b0cab4fa055024
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-02-18 16:09:24 -0800

    halfway done

---
+++ migrations/20200218144504_create_cars_table.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('sales', tbl =>{
+      tbl.increments();
+
+      tbl.string('vin', 128)
+      .notNullable()
+      .unique();
+
+    tbl.string('make',128)
+    .notNullable();
+
+
+    tbl.string('model',128)
+    .notNullable();
+
+    tbl.string('mileage', 128)
+    .notNullable()
+    .check(length(mileage) >= 10) ;
+ 
+    tbl.string('transmission', 128)
+    .defaultTo("Automatic V6");
+
+    tbl.string('title_status', 128)
+    .defaultTo("Clean");
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("sales")
+};
